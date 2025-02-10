78efe93e501e9ce9b42325b8d9567a8882a1e219
mzsoba <owt4832@gmail.com>
2025-02-10 11:58:53 +0900

    rtkFail

---
+++ src/redux/modules/Message.js
@@ -0,0 +1,12 @@
+const initialState = {
+  message: "",
+};
+
+const module = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default module;
